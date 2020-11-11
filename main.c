2166354c8d20cab202a8e719f5e0faa4795359ce
shaiBonfil <70275293+shaiBonfil@users.noreply.github.com>
2020-11-11 22:23:01 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -6,23 +6,23 @@ int main(){
   printf("please enter a number\n");
   scanf("%lf",&var );
   
-  float eToThex = Exponent(var);
-  float xToThe3 = Power(var, 3);
+  double eToThex = Exponent(var);
+  double xToThe3 = Power(var, 3);
   float fx1Add = add(eToThex, xToThe3);
   float fx1 = sub(fx1Add, 2);
 
-  float x3 = mul(3, var);
-  float x2ToThe2 = mul(2, (Power(var, 2)));
+  double x3 = mul(3, var);
+  double x2ToThe2 = mul(2, (int)(Power(var, 2)));
   float fx2 = add(x3, x2ToThe2);
 
-  int tmp = (Power(var,3));
-  float x4ToThe3 = mul(4, tmp);
-  float mulDiv5 = div(x4ToThe3, 5); 
-  float x2 = mul(2, var);
+  double tmp = (Power(var, 3));
+  double x4ToThe3 = mul(4, tmp);
+  double mulDiv5 = div(x4ToThe3, 5); 
+  double x2 = mul(2, var);
   float fx3 = sub(mulDiv5, x2);
 
-  printf("The value of f(x) = e^x + x^3 − 2 at x = %f is %f\n", var, fx1);
-  printf("The value of f(x) = 3x + 2X^2 at x = %f is %f\n", var, fx2);
-  printf("The value of f(x) = (4x^3)/5 -2x at x = %f is %f\n", var, fx3);
+  printf("The value of f(x) = e^x + x^3 − 2 at x = %f is %0.4f\n", var, fx1);
+  printf("The value of f(x) = 3x + 2X^2 at x = %f is %0.4f\n", var, fx2);
+  printf("The value of f(x) = (4x^3)/5 -2x at x = %f is %0.4f\n", var, fx3);
   return 0;
 }
