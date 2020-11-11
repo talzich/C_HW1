8c159df5c84204b585416ab5ace6fa37862df22f
shaiBonfil <70275293+shaiBonfil@users.noreply.github.com>
2020-11-11 18:01:59 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
-#include "basicMath.c"
 #include "power.c"
 
 int main(){
   double var;
-  printf("please enter a number\n" );
-  scanf("%d",&var );
-
+  printf("please enter a number\n");
+  scanf("%lf",&var );
+  
   float eToThex = Exponent(var);
   float xToThe3 = Power(var, 3);
   float fx1 = eToThex + xToThe3 - 2;
@@ -19,9 +18,8 @@ int main(){
   float x2 = 2*var;
   float fx3 = x4ToThe3Div5 - x2;
 
-  printf("The value of f(x) = e^x + x^3 − 2 at x = %d is %f\n", var, fx1);
-  printf("The value of f(x) = 3x + 2X^2 at x = %d is %f\n", var, fx1);
-  printf("The value of f(x) = (4x^3)/5 -2x at x = %d is %f\n", var, fx1);
+  printf("The value of f(x) = e^x + x^3 − 2 at x = %f is %f\n", var, fx1);
+  printf("The value of f(x) = 3x + 2X^2 at x = %f is %f\n", var, fx2);
+  printf("The value of f(x) = (4x^3)/5 -2x at x = %f is %f\n", var, fx3);
   return 0;
-
 }
