f7651f842747af14cf6f5cf0ea5c0f9b98192fe2
shaiBonfil <70275293+shaiBonfil@users.noreply.github.com>
2020-11-11 20:48:06 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -8,15 +8,18 @@ int main(){
   
   float eToThex = Exponent(var);
   float xToThe3 = Power(var, 3);
-  float fx1 = eToThex + xToThe3 - 2;
+  float fx1Add = add(eToThex, xToThe3);
+  float fx1 = sub(fx1Add, 2);
 
-  float x3 = 3*var;
-  float x2ToThe2 = 2*(Power(var, 2));
-  float fx2 = x3 + x2ToThe2;
+  float x3 = mul(3, var);
+  float x2ToThe2 = mul(2, (Power(var, 2)));
+  float fx2 = add(x3, x2ToThe2);
 
-  float x4ToThe3Div5 = (4*(Power(var,3)))/5;
-  float x2 = 2*var;
-  float fx3 = x4ToThe3Div5 - x2;
+  int tmp = (Power(var,3));
+  float x4ToThe3 = mul(4, tmp);
+  float mulDiv5 = div(x4ToThe3, 5); 
+  float x2 = mul(2, var);
+  float fx3 = sub(mulDiv5, x2);
 
   printf("The value of f(x) = e^x + x^3 − 2 at x = %f is %f\n", var, fx1);
   printf("The value of f(x) = 3x + 2X^2 at x = %f is %f\n", var, fx2);
