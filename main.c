ddfe1416fc64a6b42080ee46f5be2fda376e97fe
shaiBonfil <70275293+shaiBonfil@users.noreply.github.com>
2020-11-12 22:09:58 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
-#include "power.c"
-#include "basicMath.c"
+#include "myMath.h"
 
 int main(){
   double var;
@@ -22,10 +21,8 @@ int main(){
   double x2 = mul(2, var);
   float fx3 = sub(mulDiv5, x2);
 
-
   printf("The value of f(x) = e^x + x^3 − 2 at x = %f is %0.4f\n", var, fx1);
   printf("The value of f(x) = 3x + 2X^2 at x = %f is %0.4f\n", var, fx2);
   printf("The value of f(x) = (4x^3)/5 -2x at x = %f is %0.4f\n", var, fx3);
-
   return 0;
 }
