b27e44c7f899990e05aeadd714e8cd1c56c19250
shaiBonfil <70275293+shaiBonfil@users.noreply.github.com>
2020-11-15 16:37:42 +0200

    Add files via upload

--- power.c
+++ power.c
@@ -1,26 +1,39 @@
 #include "myMath.h"
-
-#define e 2.7182818285
-
+#define e 2.7182818285;
 double Exponent(int x){
     double res =1;
-    float i;
-    for (i = 0; i < x; i++)
+
+    if(x<0)
+    {
+      for (int i = 0; i < -x; i++)
+      {
+        res = res*e;
+      }
+      return 1/res;
+    }
+
+    for (int i = 0; i<x; i++)
     {
        res = res*e;
     }
     return res;
-    
 }
 
 
-double Power(double x , int y)
-{
+double Power(double x , int y){
     double res = 1;
-    float i;
-    for (i = 0; i < y; i++)
+    if(y<0){
+      for (int i = 0; i < -y; i++)
+      {
+          res = res*x;
+      }
+      res = 1/res;
+      return res;
+    }
+
+    for (int i = 0; i < y; i++)
     {
         res = res*x;
     }
-    return res; 
+    return res;
 }
